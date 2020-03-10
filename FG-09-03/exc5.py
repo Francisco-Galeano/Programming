40d0979fd5334f7cc367a477a4336a10357fc496
Francisco-Galeano <61284212+Francisco-Galeano@users.noreply.github.com>
2020-03-09 21:59:03 -0300

    Add files via upload

--- FG-09-03/exc5.py
+++ FG-09-03/exc5.py
@@ -5,9 +5,15 @@ ventana.config(width = 500, height = 500)
 ventana.title("Puede votar?")
 
 etiqueta_1 = tk.Label(bg = "red", fg = "black")
-etiqueta_1.place(x = 200, y = 100, width = 100, height = 50)
+etiqueta_1.place(x = 50, y = 100, width = 400, height = 50)
 
+boton_1 = tk.Button(text = "Ha ingresado la edad? Entonces CLICK AQUI!") # no olvidar el "command"
+boton_1.place(x = 50, y = 150, width = 400, height = 50)
 
+etiqueta_2 = tk.Label(text = "Ingrese en la siguiente caja la edad", bg = "red", fg = "black")
+etiqueta_2.place(x = 50, y = 200, width = 400, height = 50)
+
+caja_1 = tk.
 
 
 
